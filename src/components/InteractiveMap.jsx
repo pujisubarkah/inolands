9e4065c2432d6422ada3bbe6f87170239e4fadf9
Rezky Yayang Yakhamid <60925883+rezkyyayang@users.noreply.github.com>
2024-10-18 01:45:09 +0700

    Update InteractiveMap.jsx

--- src/components/InteractiveMap.jsx
+++ src/components/InteractiveMap.jsx
@@ -208,6 +208,7 @@ function InteractiveMap() {
                   <thead>
                     <tr style={{ backgroundColor: '#f9f9f9', textAlign: 'left' }}>
                     <th style={{ padding: '10px', borderBottom: '1px solid #ddd' }}>Judul Inovasi</th>
+                    <th style={{ padding: '10px', borderBottom: '1px solid #ddd' }}>Tahun</th>
                     <th style={{ padding: '10px', borderBottom: '1px solid #ddd' }}>Urusan</th>
                     <th style={{ padding: '10px', borderBottom: '1px solid #ddd' }}>Inovator/Deskripsi</th>
                     </tr>
@@ -216,6 +217,7 @@ function InteractiveMap() {
                     {currentInovasi.map((inovasi) => (
                     <tr key={inovasi.id} style={{ backgroundColor: '#fff', borderBottom: '1px solid #ddd' }}>
                       <td style={{ padding: '10px' }}>{inovasi.judul_inovasi}</td>
+                      <td style={{ padding: '10px' }}>{inovasi.tahun}</td>
                       <td style={{ padding: '10px' }}>{inovasi.urusan}</td>
                       <td style={{ padding: '10px' }}>{inovasi.inovator}</td>
                     </tr>
