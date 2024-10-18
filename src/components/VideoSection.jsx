18884b4b8fe876dd82e574b7c061f4cfebb77796
pujisubarkah <67720528+pujisubarkah@users.noreply.github.com>
2024-10-18 08:02:49 +0700

    Update VideoSection.jsx

--- src/components/VideoSection.jsx
+++ src/components/VideoSection.jsx
@@ -5,25 +5,25 @@ const VideoSection = () => {
   const videos = [
     {
       id: 1,
-      title: "Pengantar Public Speaking",
+      title: "Drump UP",
       views: 169,
       src: "https://youtube.com/embed/gJ0jAmLssjc",
-      description: "Pelajari dasar-dasar public speaking dalam video ini.",
+      description: "Tahap membantung kesadaran kolektif untuk berinovasi.",
       quiz: {
-        question: "Apa yang menjadi kunci sukses dalam public speaking?",
-        options: ["Persiapan", "Kepercayaan Diri", "Materi", "Semua Benar"],
+        question: "Apakah yang menjadi tujuan dari drump up",
+        options: ["Menginspirasi", "mengembangkan semangat", "memebentuk kemauan berinovasi", "Semua Benar"],
         correctAnswer: "Semua Benar",
       },
     },
     {
       id: 2,
-      title: "Public Speaking Lanjutan",
+      title: "Diagnose",
       views: 250,
       src: "https://youtube.com/embed/QExvuVbnMwM",
       description: "Tingkatkan kemampuan public speaking Anda dengan teknik lanjutan.",
       quiz: {
-        question: "Apa teknik yang sering digunakan dalam public speaking?",
-        options: ["Storytelling", "Debate", "Dialogue", "Semua Benar"],
+        question: "Apa yang dimaksud dengan diagnose",
+        options: ["identifikasi kesenjangan kondisi saat ini dengan seharusnya", "fasilitasi dalam menemukan ide inovasi", "mengali potensi yang dimiliki untuk mengembangkan", "Semua Benar"],
         correctAnswer: "Semua Benar",
       },
     },
@@ -77,11 +77,20 @@ const VideoSection = () => {
 
   return (
     <section className="video-section">
-      <h1>Laboaratorium Inovasi</h1>
+     <h1 style={{ fontFamily: 'Poppins, sans-serif', fontWeight: 'bold', fontSize: '2rem', textAlign: 'center', margin: '20px 0' }}>
+      LABORATORIUM INOVASI
+    </h1>
+    <h2 style={{ fontFamily: 'Poppins, sans-serif', fontSize: '1,5rem', textAlign: 'justify', margin: '20px 0' }}> Model Laboratorium Inovasi  memiliki 5 tahapan atau yang dikenal dengan istilah 5D. 
+      Tahap pertama adalah Drum Up yang digunakan untuk membangkitkan semangat untuk ber-inovasi. 
+      Tahap kedua adalah Diagnose sebagai kegiatan mengidentifikasi potensi dan kemudian memunculkan ide inovasi. 
+      Tahap selanjutnya adalah Design yang akan mengupas tentang bagaimana mendesain sebuah inovasi sampai dengan rencana aksi yang akan dilakukan. 
+      Masuk tahap keempat ada Delivery yang diisi dengan pemantauan rencana aksi inovasi yang dilakukan. Tahap kelima adalah Display untuk memamerkan 
+      hasil inovasi yang dilakukan oleh pemerintah daerah, biasanya dikemas dalam sebuah acara bertajuk Gelar Karya Inovasi.</h2>
       <div className="video-wrapper">
         {videos.map(video => (
           <div className="video-item" key={video.id}>
             <h2>{video.title}</h2>
+          <p>{video.subtitle}</p>
             <iframe
               width="500"
               height="300"
