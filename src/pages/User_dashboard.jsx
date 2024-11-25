381ff5cb40e6c6702425656b6d0e2a861ab3402e
pujisubarkah <67720528+pujisubarkah@users.noreply.github.com>
2024-11-25 16:07:16 +0700

    Add files via upload

---
+++ src/pages/User_dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function UserDashboard() {
+  return (
+    <div>
+      <h1 className="text-xl font-bold">Welcome to User Dashboard</h1>
+      <p>Ini adalah halaman dashboard user. Sesuaikan kontennya sesuai kebutuhan Anda!</p>
+    </div>
+  );
+}
+
+export default UserDashboard;
