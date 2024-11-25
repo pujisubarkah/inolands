105c8e5af32732dd5441eedb19a15acad7dc668e
pujisubarkah <67720528+pujisubarkah@users.noreply.github.com>
2024-11-25 16:08:42 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,38 @@
-// App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AdminLayout from './layouts/AdminLayout';  // Import AdminLayout
-import Login from './pages/login';  // Import the Login component
-import Dashboard from './pages/dashboard'; // Assuming Dashboard component for main page
-import ListUnit from './components/list_unit'; // ListUnit page
-import ListAll from './components/list_all_pegawai'; // ListAll page
-import Pensiun from './pegawai_inaktif/pensiun'; // Pensiun page
+import AdminLayout from './layouts/AdminLayout';
+import UserLayout from './layouts/UserLayout'; // Import UserLayout
+import Login from './pages/login';
+import Dashboard from './pages/dashboard';
+import ListUnit from './components/list_unit';
+import ListAll from './components/list_all_pegawai';
+import Pensiun from './pegawai_inaktif/pensiun';
 import MENINGGAL from './pegawai_inaktif/meninggal';
 import PINDAH from './pegawai_inaktif/pindah';
+import UserDashboard from './pages/user_dashboard'; // Import placeholder
 
-// Main app component
 function AppContent() {
   return (
     <div className="flex flex-col h-screen bg-gray-100">
       <Routes>
-        {/* Route untuk Login tanpa layout */}
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
 
-        {/* Semua route yang memerlukan Sidebar, Header, dan Navbar dibungkus dalam AdminLayout */}
+        {/* Admin Layout */}
         <Route path="/dashboard" element={<AdminLayout><Dashboard /></AdminLayout>} />
         <Route path="/list_unit" element={<AdminLayout><ListUnit /></AdminLayout>} />
         <Route path="/list_all_pegawai" element={<AdminLayout><ListAll /></AdminLayout>} />
-        <Route path="/list_all_pegawai/:unit_kerja_id" element={<AdminLayout><ListAll /></AdminLayout>} />
-        <Route path="/home" element={<AdminLayout><Dashboard /></AdminLayout>} />
         <Route path="/pegawai_inaktif/pensiun" element={<AdminLayout><Pensiun /></AdminLayout>} />
         <Route path="/pegawai_inaktif/meninggal" element={<AdminLayout><MENINGGAL /></AdminLayout>} />
         <Route path="/pegawai_inaktif/pindah" element={<AdminLayout><PINDAH /></AdminLayout>} />
 
-        {/* Rute lainnya bisa ditambahkan */}
+        {/* User Layout */}
+        <Route path="/user/dashboard" element={<UserLayout><UserDashboard /></UserLayout>} />
       </Routes>
     </div>
   );
 }
 
-// Wrapper component for BrowserRouter
 function App() {
   return (
     <Router>
