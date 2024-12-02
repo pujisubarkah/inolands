cbfbc7e4b2dea360176a0d9f238ae40bd2bdb412
Rezky Yayang Yakhamid <60925883+rezkyyayang@users.noreply.github.com>
2024-12-02 12:27:55 +0700

    Update pdflistdetail.jsx

--- src/pengetahuan/pdflistdetail.jsx
+++ src/pengetahuan/pdflistdetail.jsx
@@ -38,12 +38,38 @@ const PdfDetail = () => {
   }
 
   return (
-    <div className="news-detail">
+    <div style={{
+      padding: '30px',
+      maxWidth: '1200px',
+      margin: '20px auto',
+      background: '#f9f9f9',
+      borderRadius: '12px',
+      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+      transition: 'box-shadow 0.3s ease',
+    }}>
       {inovasiDetail && (
         <>
-          <h1 className="news-title">{inovasiDetail.pdf_judul}</h1>
-          <p className="news-date">{inovasiDetail.pdf_publisher}</p>
-          <iframe src={inovasiDetail.pdf_url} width="720" height="1000" allow="autoplay"></iframe>
+          <h1 style={{ fontSize: '2.5em', fontWeight: 'bold', fontFamily: 'Georgia' }}>{inovasiDetail.pdf_judul}</h1>
+          <p style={{ fontSize: '1.2em', color: '#555' }}>{inovasiDetail.pdf_publisher}</p>
+          <br/>
+          <div style={{ position: 'relative', paddingTop: 'max(60%, 324px)', width: '100%', height: '0' }}>
+            <iframe
+              style={{
+                position: 'absolute',
+                border: 'none',
+                width: '100%',
+                height: '100%',
+                left: '0',
+                top: '0',
+              }}
+              src={inovasiDetail.pdf_url}
+              seamless="seamless"
+              scrolling="no"
+              frameBorder="0"
+              allowTransparency="true"
+              allowFullScreen="true"
+            ></iframe>
+          </div>
         </>
       )}
     </div>
