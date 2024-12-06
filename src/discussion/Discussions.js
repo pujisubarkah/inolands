f7ea8b78a5940d32b47bf29b472e1a7e0467c42b
Rezky Yayang Yakhamid <60925883+rezkyyayang@users.noreply.github.com>
2024-12-06 16:21:53 +0700

    Update Discussions.js

--- src/discussion/Discussions.js
+++ src/discussion/Discussions.js
@@ -1,10 +1,8 @@
-import AddPost from './CreateDiscussion';
 import Forum from './DetailDiscussion';
 
 const ForumPage = () => {
   return (
     <div>
-      <AddPost />
       <Forum />
     </div>
   );
