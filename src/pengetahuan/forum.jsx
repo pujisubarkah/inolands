b3c6b8f786a9a9b7b7449f8e0c1ee2e91f6d3b0c
Rezky Yayang Yakhamid <60925883+rezkyyayang@users.noreply.github.com>
2024-12-09 08:36:51 +0700

    Update forum.jsx

--- src/pengetahuan/forum.jsx
+++ src/pengetahuan/forum.jsx
@@ -1,10 +1,8 @@
-import AddPost from '../discussion/CreateDiscussion';
 import Forum from '../discussion/DetailDiscussion';
 
 const ForumPage = () => {
   return (
     <div>
-      <AddPost />
       <Forum />
     </div>
   );
