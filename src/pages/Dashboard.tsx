807c06b00c2b8154e9e3ec5ce9967c2babcc10b7
pujisubarkah <67720528+pujisubarkah@users.noreply.github.com>
2025-04-29 09:30:57 +0700

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,5 @@
 // src/components/dashboard.jsx
 import React, { useState } from 'react';
-import Diagnose from '../dashboard/Diagnose';
 import Ide from '../dashboard/Ide';
 import Rencana from '../dashboard/Rencana';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -10,17 +9,13 @@ const Dashboard = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [activeComponent, setActiveComponent] = React.useState('IdeInovasi');
 
-
-  // Toggle sidebar visibility
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
 
   return (
     <div style={{ display: 'flex' }}>
-
-
-<div style={{ 
+      <div style={{ 
         width: isSidebarOpen ? '250px' : '50px', 
         transition: 'width 0.3s', 
         overflow: 'hidden', 
@@ -69,26 +64,6 @@ const Dashboard = () => {
           </li>
           <li style={{ marginBottom: '10px' }}>
             <button 
-              onClick={() => setActiveComponent('Diagnose')} 
-              style={{
-                backgroundColor: '#444',
-                color: '#fff',
-                border: 'none',
-                padding: '10px 20px',
-                cursor: 'pointer',
-                borderRadius: '5px',
-                width: '100%',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: isSidebarOpen ? 'flex-start' : 'center'
-              }}
-            >
-              <FontAwesomeIcon icon={faTableCells} />
-              {isSidebarOpen && <span style={{ marginLeft: '10px' }}>Diagnose Inovasi</span>}
-            </button>
-          </li>
-          <li style={{ marginBottom: '10px' }}>
-            <button 
               onClick={() => setActiveComponent('RencanaAksi')} 
               style={{
                 backgroundColor: '#444',
@@ -110,11 +85,9 @@ const Dashboard = () => {
         </ul>
       </div>
       <div style={{ flex: 1, padding: '10px' }}>
-        {activeComponent === 'Diagnose' && <Diagnose />}
         {activeComponent === 'IdeInovasi' && <Ide />}
         {activeComponent === 'RencanaAksi' && <Rencana />}
       </div>
-
     </div>
   );
 };
